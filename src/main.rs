0678880b3c8193b5ffe06902cc9d6000812165dd
iDeBugger Freeman <me@idebugger.io>
2022-07-07 22:09:34 +0300

    Part 2 solution

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,43 @@ fn main() {
     let example_polymer = include_str!("./data/example.txt").to_string();
     println!(
         "Example reaction result length: {}",
-        simulate_reaction(example_polymer).len()
+        simulate_reaction(&example_polymer).len()
+    );
+
+    println!(
+        "Example shortest reaction length: {}",
+        find_problem_unit(&example_polymer)
     );
 
     let input_polymer = include_str!("./data/input.txt").to_string();
     println!(
         "Input reaction result length: {}",
-        simulate_reaction(input_polymer).len()
+        simulate_reaction(&input_polymer).len()
+    );
+
+    println!(
+        "Example shortest reaction length: {}",
+        find_problem_unit(&input_polymer)
     );
 }
 
-fn simulate_reaction(polymer: String) -> String {
+fn find_problem_unit(polymer: &String) -> usize {
+    let mut shortest_polymer_len = usize::MAX;
+    for chr in 'a'..'z' {
+        let reacted_length = simulate_reaction(
+            &polymer
+                .replace(&chr.to_lowercase().to_string(), "")
+                .replace(&chr.to_uppercase().to_string(), ""),
+        )
+        .len();
+        if reacted_length < shortest_polymer_len {
+            shortest_polymer_len = reacted_length;
+        }
+    }
+    shortest_polymer_len
+}
+
+fn simulate_reaction(polymer: &String) -> String {
     let mut stack: Vec<char> = vec![];
     for chr in polymer.chars() {
         if let Some(stack_top) = stack.last() {
